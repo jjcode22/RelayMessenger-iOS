81576dd9f1bd05d05f580a53145ac22f581f4124
jjcode22 <jjmcoding31@gmail.com>
2024-03-19 23:39:12 +0530

    firebase

--- RelayMessenger/Module/API/FileUploader.swift
+++ RelayMessenger/Module/API/FileUploader.swift
@@ -8,11 +8,34 @@
 import Foundation
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
 struct FileUploader {
     static func uploadImage(image: UIImage, completion: @escaping(String) -> Void){
-        guard let imageData = image.jpegData(withCompressionQuality: 0.75) else {return}
-        let uid = Auth.auth().currentUser?.uid
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else {return}
+        let uid = Auth.auth().currentUser?.uid ?? "/profileImages/"
+        
+        
+        let fileName = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/\(uid)/\(fileName)")
+        
+        ref.putData(imageData, metadata: nil) { metadata,error in
+            if let error = error{
+                print(error.localizedDescription)
+                return
+            }
+            
+            ref.downloadURL { url, error in
+                if let error = error{
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let fileURL = url?.absoluteString else {return}
+                completion(fileURL)
+            }
+        }
+
         
     }
     
